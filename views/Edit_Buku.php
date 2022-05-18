603544c724305c63da5cc1e1264612717c2855d8
justjhnd <91574382+justjhnd@users.noreply.github.com>
2022-05-18 19:00:20 +0700

    Add files via upload

---
+++ views/Edit_Buku.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Edit Buku</title>
+</head>
+<body>
+	<h3>Edit BUKU</h3>
+	<table>
+		<tr>
+			<td>Kode Buku</td>
+			<td>:</td>
+			<td><input type = "teks" name = "Kode Buku" readonly></td>
+		</tr>
+		<tr>
+			<td>Judul Buku</td>
+			<td>:</td>
+			<td><input type = "teks" name = "Judul Buku"></td>
+		</tr>
+		<tr>
+			<td>Penerbit</td>
+			<td>:</td>
+			<td><input type = "teks" name = "Penerbit"></td>
+		</tr>
+		<tr>
+			<td colspan="3"><button type = "Submit">update Buku</button></td>
+		</tr>
+	</table>
+		
+</body>
+</html>
---
+++ views/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+</head>
+<body>
+	<h1> CRUD Data BUKU </h1>
+
+	<a href = ''> Tambah Data</a>
+	<br></br>
+	<table border = '1'>
+		<tr>
+			<td>No</td>
+			<td>Kode Buku</td>
+			<td>Judul Buku</td>
+			<td>Penerbit</td>
+			<td>Aksi</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>10</td>
+			<td>dillan 1990</td>
+			<td>Pidi Baiq</td>
+			<td> <a href = ''> edit</a>| <a href = '' >delet</a> </td>
+		</tr> 
+		<tr>
+			<td>2</td>
+			<td>20</td>
+			<td>dillan 1991</td>
+			<td>Pidi Baiq</td>
+			<td> <a href = ''> edit</a>| <a href = '' >delet</a> </td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>30</td>
+			<td>Naruto</td>
+			<td>Masashi Kishimoto</td>
+			<td> <a href = ''> edit</a>| <a href = '' >delet</a> </td>
+		</tr>
+	</table>
+
+</body>
+</html>
