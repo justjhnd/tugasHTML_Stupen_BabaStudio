3676b17ac5429f6f4d34867e5abc0840ddab7b71
justjhnd <91574382+justjhnd@users.noreply.github.com>
2022-05-18 18:56:37 +0700

    Add files via upload

---
+++ tambah_buku.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tambah Buku</title>
+</head>
+<body>
+	<h3>Halaman Tambah BUKU</h3>
+	<table>
+		<tr>
+			<td>Judul Buku</td>
+			<td>:</td>
+			<td><input type = "teks" name = "Judul Buku"></td>
+		</tr>
+		<tr>
+			<td>Penerbit</td>
+			<td>:</td>
+			<td><input type = "teks" name = "Penerbit"></td>
+		</tr>
+		<tr>
+			<td colspan="3"><button type = "Submit">Tambah Buku</button></td>
+
+		</tr>
+	</table>
+		
+</body>
+</html>
